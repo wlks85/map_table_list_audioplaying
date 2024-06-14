af4dd8651abb976e87e4acfbdfdfffc692b82f6b
A S M MEHEDI HASAN SHUVO <hasanasmmehedi@gmail.com>
2024-06-14 11:56:51 +0600

    server

--- frontend/src/services/WordService.ts
+++ frontend/src/services/WordService.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 const env = import.meta.env;
-const host: string = "192.168.0.109";//env.VITE_API_ROOT || "192.168.0.109";
-const port: string = ":8082";//env.VITE_NODE_ENV === 'local' ? ":8082": "";
-const scheme: string = env.VITE_NODE_ENV === 'local' ? 'http' : 'http';
+const host: string = env.VITE_API_ROOT || "localhost";
+const port: string = env.VITE_NODE_ENV === 'local' ? ":8082": "";
+const scheme: string = process.env.VITE_NODE_ENV === 'local' ? 'http' : 'https';
 const BASE_URL = `${scheme}://${host}${port}/api/v1`;
 export const WordService = {
     async getWords() {
-        alert(BASE_URL);
         const {status, data} = await axios.get(`${BASE_URL}/words`);
         if(status === 200) return data;
         else throw new Error("Could not fetch data")
