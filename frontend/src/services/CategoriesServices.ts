5636ed094e01921462c8178666da1e9267bf1313
wlks85 <wingleeatoz8@gmail.com>
2024-09-30 14:37:11 -0400

    build_10

--- frontend/src/services/CategoriesServices.ts
+++ frontend/src/services/CategoriesServices.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const env = import.meta.env;
 const host: string = env.VITE_API_ROOT || "audio.dialektatlas.ch";
 // const port: string = env.VITE_NODE_ENV === "local" ? ":8000" : "";
-const port = "8001";
+const port = ":8001";
 // const scheme: string = env.VITE_NODE_ENV === "local" ? "http" : "https";
 const scheme = "https";
 const BASE_URL = `${scheme}://${host}${port}/api/v1`;
